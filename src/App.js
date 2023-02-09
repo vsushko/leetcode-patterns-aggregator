6b839181016c14bf8814e623b347f9ad23351998
Vasiliy Sushko <vasiliy.sushko@gmail.com>
2023-02-09 14:48:07 +0300

    added simple table and basic markup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <h1>Hello there!</h1>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const problems =
+  [
+    {
+      name: "Contains Duplicate",
+      link: "https://leetcode.com/problems/contains-duplicate/",
+      patterns: ["Arrays"],
+      difficulty: "Easy"
+    },
+    {
+      name: "Missing Number",
+      link: "https://leetcode.com/problems/missing-number/",
+      patterns: ["Arrays", "Bit Manipulation"],
+      difficulty: "Easy"
+    },
+  ];
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div>
+          <nav className="navbar navbar-light bg-light">
+            <div className="ps-3">
+              <a className="navbar-brand" href="#">Leetcode patterns aggregator</a>
+            </div>
+          </nav>
+        </div>
+        <div className="container-md pt-3">
+          <div>
+            <table class="table table-sm table-bordered table-hover table-striped">
+              <thead>
+                <tr>
+                  <th scope="col">#</th>
+                  <th scope="col">Problem</th>
+                  <th scope="col">Pattern</th>
+                  <th scope="col">Difficulty</th>
+                </tr>
+              </thead>
+              <tbody>{
+                problems.map((problem, idx) => (
+                  <tr>
+                    <th scope="row">{idx + 1}</th>
+                    <td><a className="navbar-brand" href={problem.link}>{problem.name}</a></td>
+                    <td>{problem.patterns.join(', ')}</td>
+                    <td>{problem.difficulty}</td>
+                  </tr>
+                ))
+              }
+              </tbody>
+            </table>
+          </div>
+        </div>
+        <div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
