6eb92d1566a8c732fe0bda747380d2bbbde4b013
Vasiliy Sushko <vasiliy.sushko@gmail.com>
2023-02-13 02:21:32 +0300

    neetcode 150

---
+++ src/store/neetcode-one-hundred-fifty.js
@@ -0,0 +1,549 @@
+export const NEETCODE_150_LIST =
+  [
+    {
+      name: "Contains Duplicate",
+      link: "https://leetcode.com/problems/contains-duplicate/",
+      topic: "Arrays & Hashing",
+      patterns: [],
+      difficulty: "Easy"
+    },
+    {
+      name: "Valid Anagram",
+      link: "https://leetcode.com/problems/valid-anagram/",
+      topic: "Arrays & Hashing",
+      patterns: [],
+      difficulty: "Easy"
+    },
+    {
+      name: "Two Sum",
+      link: "https://leetcode.com/problems/two-sum/",
+      topic: "Arrays & Hashing",
+      patterns: [],
+      difficulty: "Easy"
+    },
+    {
+      name: "Group Anagrams",
+      link: "https://leetcode.com/problems/group-anagrams/",
+      patterns: [],
+      topic: "Arrays & Hashing",
+      difficulty: "Medium"
+    },
+    {
+      name: "Top K Frequent Elements",
+      link: "https://leetcode.com/problems/top-k-frequent-elements/",
+      patterns: [],
+      topic: "Arrays & Hashing",
+      difficulty: "Medium"
+    },
+    {
+      name: "Product of Array Except Self",
+      link: "https://leetcode.com/problems/product-of-array-except-self/",
+      patterns: [],
+      topic: "Arrays & Hashing",
+      difficulty: "Medium"
+    },
+    {
+      name: "Valid Sudoku",
+      link: "https://leetcode.com/problems/valid-sudoku/",
+      patterns: [],
+      topic: "Arrays & Hashing",
+      difficulty: "Medium"
+    },
+    {
+      name: "Encode And Decode Strings",
+      link: "https://leetcode.com/problems/encode-and-decode-strings/",
+      patterns: [],
+      topic: "Arrays & Hashing",
+      difficulty: "Medium"
+    },
+    {
+      name: "Longest Consecutive Sequence",
+      link: "https://leetcode.com/problems/longest-consecutive-sequence/",
+      patterns: [],
+      topic: "Arrays & Hashing",
+      difficulty: "Medium"
+    },
+    {
+      name: "Valid Palindrome",
+      link: "https://leetcode.com/problems/valid-palindrome/",
+      patterns: [],
+      topic: "Two Pointers",
+      difficulty: "Easy"
+    },
+    {
+      name: "Two Sum II Input Array Is Sorted",
+      link: "https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/",
+      patterns: [],
+      topic: "Two Pointers",
+      difficulty: "Medium"
+    },
+    {
+      name: "3Sum",
+      link: "https://leetcode.com/problems/3sum/",
+      patterns: [],
+      topic: "Two Pointers",
+      difficulty: "Medium"
+    },
+    {
+      name: "Container With Most Water",
+      link: "https://leetcode.com/problems/container-with-most-water/",
+      patterns: [],
+      topic: "Two Pointers",
+      difficulty: "Medium"
+    },
+    {
+      name: "Trapping Rain Water",
+      link: "https://leetcode.com/problems/trapping-rain-water/",
+      patterns: [],
+      topic: "Two Pointers",
+      difficulty: "Hard"
+    },
+    {
+      name: "Best Time to Buy And Sell Stock",
+      link: "https://leetcode.com/problems/best-time-to-buy-and-sell-stock/",
+      patterns: [],
+      topic: "Sliding Window",
+      difficulty: "Easy"
+    },
+    {
+      name: "Longest Substring Without Repeating Characters",
+      link: "https://leetcode.com/problems/longest-substring-without-repeating-characters/",
+      patterns: [],
+      topic: "Sliding Window",
+      difficulty: "Medium"
+    },
+    {
+      name: "Longest Repeating Character Replacement",
+      link: "https://leetcode.com/problems/longest-repeating-character-replacement/",
+      patterns: [],
+      topic: "Sliding Window",
+      difficulty: "Medium"
+    },
+    {
+      name: "Minimum Window Substring",
+      link: "https://leetcode.com/problems/minimum-window-substring/",
+      patterns: [],
+      topic: "Sliding Window",
+      difficulty: "Hard"
+    },
+    {
+      name: "Valid Parentheses",
+      link: "https://leetcode.com/problems/valid-parentheses/",
+      patterns: [],
+      topic: "Stack",
+      difficulty: "Easy"
+    },
+    {
+      name: "Find Minimum In Rotated Sorted Array",
+      link: "https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/",
+      patterns: [],
+      topic: "Binary Search",
+      difficulty: "Medium"
+    },
+    {
+      name: "Search In Rotated Sorted Array",
+      link: "https://leetcode.com/problems/search-in-rotated-sorted-array/",
+      patterns: [],
+      topic: "Binary Search",
+      difficulty: "Medium"
+    },
+    {
+      name: "Reverse Linked List",
+      link: "https://leetcode.com/problems/reverse-linked-list/",
+      patterns: [],
+      topic: "Linked List",
+      difficulty: "Easy"
+    },
+    {
+      name: "Merge Two Sorted Lists",
+      link: "https://leetcode.com/problems/merge-two-sorted-lists/",
+      patterns: [],
+      topic: "Linked List",
+      difficulty: "Easy"
+    },
+    {
+      name: "Reorder List",
+      link: "https://leetcode.com/problems/reorder-list/",
+      patterns: [],
+      topic: "Linked List",
+      difficulty: "Medium"
+    },
+    {
+      name: "Remove Nth Node From End of List",
+      link: "https://leetcode.com/problems/remove-nth-node-from-end-of-list/",
+      patterns: [],
+      topic: "Linked List",
+      difficulty: "Medium"
+    },
+    {
+      name: "Linked List Cycle",
+      link: "https://leetcode.com/problems/linked-list-cycle/",
+      patterns: [],
+      topic: "Linked List",
+      difficulty: "Easy"
+    },
+    {
+      name: "Merge K Sorted Lists",
+      link: "https://leetcode.com/problems/merge-k-sorted-lists/",
+      patterns: [],
+      topic: "Linked List",
+      difficulty: "Hard"
+    },
+    {
+      name: "Invert Binary Tree",
+      link: "https://leetcode.com/problems/invert-binary-tree/",
+      patterns: [],
+      topic: "Trees",
+      difficulty: "Easy"
+    },
+    {
+      name: "Maximum Depth of Binary Tree",
+      link: "https://leetcode.com/problems/maximum-depth-of-binary-tree/",
+      patterns: [],
+      topic: "Trees",
+      difficulty: "Easy"
+    },
+    {
+      name: "Same Tree",
+      link: "https://leetcode.com/problems/same-tree/",
+      patterns: [],
+      topic: "Trees",
+      difficulty: "Easy"
+    },
+    {
+      name: "Subtree of Another Tree",
+      link: "https://leetcode.com/problems/subtree-of-another-tree/",
+      patterns: [],
+      topic: "Trees",
+      difficulty: "Easy"
+    },
+    {
+      name: "Lowest Common Ancestor of a Binary Search Tree",
+      link: "https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/",
+      patterns: [],
+      topic: "Trees",
+      difficulty: "Medium"
+    },
+    {
+      name: "Binary Tree Level Order Traversal",
+      link: "https://leetcode.com/problems/binary-tree-level-order-traversal/",
+      patterns: [],
+      topic: "Trees",
+      difficulty: "Medium"
+    },
+    {
+      name: "Validate Binary Search Tree",
+      link: "https://leetcode.com/problems/validate-binary-search-tree/",
+      patterns: [],
+      topic: "Trees",
+      difficulty: "Medium"
+    },
+    {
+      name: "Kth Smallest Element In a Bst",
+      link: "https://leetcode.com/problems/kth-smallest-element-in-a-bst/",
+      patterns: [],
+      topic: "Trees",
+      difficulty: "Medium"
+    },
+    {
+      name: "Construct Binary Tree From Preorder And Inorder Traversal",
+      link: "https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/",
+      patterns: [],
+      topic: "Trees",
+      difficulty: "Medium"
+    },
+    {
+      name: "Binary Tree Maximum Path Sum",
+      link: "https://leetcode.com/problems/binary-tree-maximum-path-sum/",
+      patterns: [],
+      topic: "Trees",
+      difficulty: "Hard"
+    },
+    {
+      name: "Serialize And Deserialize Binary Tree",
+      link: "https://leetcode.com/problems/serialize-and-deserialize-binary-tree/",
+      patterns: [],
+      topic: "Trees",
+      difficulty: "Hard"
+    },
+    {
+      name: "Implement Trie Prefix Tree",
+      link: "https://leetcode.com/problems/implement-trie-prefix-tree/",
+      patterns: [],
+      topic: "Tries",
+      difficulty: "Medium"
+    },
+    {
+      name: "Design Add And Search Words Data Structure",
+      link: "https://leetcode.com/problems/design-add-and-search-words-data-structure/",
+      patterns: [],
+      topic: "Trees",
+      difficulty: "Medium"
+    },
+    {
+      name: "Word Search II",
+      link: "https://leetcode.com/problems/word-search-ii/",
+      patterns: [],
+      topic: "Trees",
+      difficulty: "Hard"
+    },
+    {
+      name: "Find Median From Data Stream",
+      link: "https://leetcode.com/problems/find-median-from-data-stream/",
+      patterns: [],
+      topic: "Heap / Priority Queue",
+      difficulty: "Hard"
+    },
+    {
+      name: "Combination Sum",
+      link: "https://leetcode.com/problems/combination-sum/",
+      patterns: [],
+      topic: "Backtracking",
+      difficulty: "Medium"
+    },
+    {
+      name: "Word Search",
+      link: "https://leetcode.com/problems/word-search/",
+      patterns: [],
+      topic: "Backtracking",
+      difficulty: "Medium"
+    },
+    {
+      name: "Number of Islands",
+      link: "https://leetcode.com/problems/number-of-islands/",
+      patterns: [],
+      topic: "Graph",
+      difficulty: "Medium"
+    },
+    {
+      name: "Clone Graph",
+      link: "https://leetcode.com/problems/clone-graph/",
+      patterns: [],
+      topic: "Graph",
+      difficulty: "Medium"
+    },
+    {
+      name: "Pacific Atlantic Water Flow",
+      link: "https://leetcode.com/problems/pacific-atlantic-water-flow/",
+      patterns: [],
+      topic: "Graph",
+      difficulty: "Medium"
+    },
+    {
+      name: "Course Schedule",
+      link: "https://leetcode.com/problems/course-schedule/",
+      patterns: [],
+      topic: "Graph",
+      difficulty: "Medium"
+    },
+    {
+      name: "Number of Connected Components In An Undirected Graph",
+      link: "https://leetcode.com/problems/number-of-connected-components-in-an-undirected-graph/",
+      patterns: [],
+      topic: "Graph",
+      difficulty: "Medium"
+    },
+    {
+      name: "Graph Valid Tree",
+      link: "https://leetcode.com/problems/graph-valid-tree/",
+      patterns: [],
+      topic: "Graph",
+      difficulty: "Medium"
+    },
+    {
+      name: "Alien Dictionary",
+      link: "https://leetcode.com/problems/alien-dictionary/",
+      patterns: [],
+      topic: "Advanced Graphs",
+      difficulty: "Hard"
+    },
+    {
+      name: "Climbing Stairs",
+      link: "https://leetcode.com/problems/climbing-stairs/",
+      patterns: [],
+      topic: "1-D Dynamic Programming",
+      difficulty: "Easy"
+    },
+    {
+      name: "House Robber",
+      link: "https://leetcode.com/problems/house-robber/",
+      patterns: [],
+      topic: "1-D Dynamic Programming",
+      difficulty: "Medium"
+    },
+    {
+      name: "House Robber II",
+      link: "https://leetcode.com/problems/house-robber-ii/",
+      patterns: [],
+      topic: "1-D Dynamic Programming",
+      difficulty: "Medium"
+    },
+    {
+      name: "Longest Palindromic Substring",
+      link: "https://leetcode.com/problems/longest-palindromic-substring/",
+      patterns: [],
+      topic: "1-D Dynamic Programming",
+      difficulty: "Medium"
+    },
+    {
+      name: "Palindromic Substrings",
+      link: "https://leetcode.com/problems/palindromic-substrings/",
+      patterns: [],
+      topic: "1-D Dynamic Programming",
+      difficulty: "Medium"
+    },
+    {
+      name: "Decode Ways",
+      link: "https://leetcode.com/problems/decode-ways/",
+      patterns: [],
+      topic: "1-D Dynamic Programming",
+      difficulty: "Medium"
+    },
+    {
+      name: "Coin Change",
+      link: "https://leetcode.com/problems/coin-change/",
+      patterns: [],
+      topic: "1-D Dynamic Programming",
+      difficulty: "Medium"
+    },
+    {
+      name: "Maximum Product Subarray",
+      link: "https://leetcode.com/problems/maximum-product-subarray/",
+      patterns: [],
+      topic: "1-D Dynamic Programming",
+      difficulty: "Medium"
+    },
+    {
+      name: "Word Break",
+      link: "https://leetcode.com/problems/word-break/",
+      patterns: [],
+      topic: "1-D Dynamic Programming",
+      difficulty: "Medium"
+    },
+    {
+      name: "Longest Increasing Subsequence",
+      link: "https://leetcode.com/problems/longest-increasing-subsequence/",
+      patterns: [],
+      topic: "1-D Dynamic Programming",
+      difficulty: "Medium"
+    },
+    {
+      name: "Unique Paths",
+      link: "https://leetcode.com/problems/unique-paths/",
+      patterns: [],
+      topic: "2-D Dynamic Programming",
+      difficulty: "Medium"
+    },
+    {
+      name: "Longest Common Subsequence",
+      link: "https://leetcode.com/problems/longest-common-subsequence/",
+      patterns: [],
+      topic: "2-D Dynamic Programming",
+      difficulty: "Medium"
+    },
+    {
+      name: "Maximum Subarray",
+      link: "https://leetcode.com/problems/maximum-subarray/",
+      patterns: [],
+      topic: "Greedy",
+      difficulty: "Medium"
+    },
+    {
+      name: "Jump Game",
+      link: "https://leetcode.com/problems/jump-game/",
+      patterns: [],
+      topic: "Greedy",
+      difficulty: "Medium"
+    },
+    {
+      name: "Insert Interval",
+      link: "https://leetcode.com/problems/insert-interval/",
+      patterns: [],
+      topic: "Intervals",
+      difficulty: "Medium"
+    },
+    {
+      name: "Merge Intervals",
+      link: "https://leetcode.com/problems/merge-intervals/",
+      patterns: [],
+      topic: "Intervals",
+      difficulty: "Medium"
+    },
+    {
+      name: "Non Overlapping Intervals",
+      link: "https://leetcode.com/problems/non-overlapping-intervals/",
+      patterns: [],
+      topic: "Intervals",
+      difficulty: "Medium"
+    },
+    {
+      name: "Meeting Rooms",
+      link: "https://leetcode.com/problems/meeting-rooms/",
+      patterns: [],
+      topic: "Intervals",
+      difficulty: "Easy"
+    },
+    {
+      name: "Meeting Rooms II",
+      link: "https://leetcode.com/problems/meeting-rooms-ii/",
+      patterns: [],
+      topic: "Intervals",
+      difficulty: "Medium"
+    },
+    {
+      name: "Rotate Image",
+      link: "https://leetcode.com/problems/rotate-image/",
+      patterns: [],
+      topic: "Math & Geometry",
+      difficulty: "Medium"
+    },
+    {
+      name: "Spiral Matrix",
+      link: "https://leetcode.com/problems/spiral-matrix/",
+      patterns: [],
+      topic: "Math & Geometry",
+      difficulty: "Medium"
+    },
+    {
+      name: "Set Matrix Zeroes",
+      link: "https://leetcode.com/problems/set-matrix-zeroes/",
+      patterns: [],
+      topic: "Math & Geometry",
+      difficulty: "Medium"
+    },
+    {
+      name: "Number of 1 Bits",
+      link: "https://leetcode.com/problems/number-of-1-bits/",
+      patterns: [],
+      topic: "Bit Manipulation",
+      difficulty: "Easy"
+    },
+    {
+      name: "Counting Bits",
+      link: "https://leetcode.com/problems/counting-bits/",
+      patterns: [],
+      topic: "Bit Manipulation",
+      difficulty: "Easy"
+    },
+    {
+      name: "Reverse Bits",
+      link: "https://leetcode.com/problems/reverse-bits/",
+      patterns: [],
+      topic: "Bit Manipulation",
+      difficulty: "Easy"
+    },
+    {
+      name: "Missing Number",
+      link: "https://leetcode.com/problems/missing-number/",
+      patterns: [],
+      topic: "Bit Manipulation",
+      difficulty: "Easy"
+    },
+    {
+      name: "Sum of Two Integers",
+      link: "https://leetcode.com/problems/sum-of-two-integers/",
+      patterns: [],
+      topic: "Bit Manipulation",
+      difficulty: "Medium"
+    },
+  ];
