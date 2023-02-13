e5e1ae3450d672524fe3f6a8a3d7b49106f28c99
Vasiliy Sushko <vasiliy.sushko@gmail.com>
2023-02-14 01:07:48 +0300

    added select list input

--- src/store/seanprashad.js
+++ src/store/seanprashad.js
@@ -4,21 +4,109 @@ const seanprashadList =
       name: "Contains Duplicate",
       link: "https://leetcode.com/problems/contains-duplicate/",
       patterns: ["Arrays"],
+      topic: "Arrays",
       difficulty: "Easy"
     },
     {
       name: "Missing Number",
       link: "https://leetcode.com/problems/missing-number/",
-      patterns: ["Arrays", "Bit Manipulation"],
+      patterns: ["Arrays"],
+      topic: "Arrays",
       difficulty: "Easy"
     },
     {
       name: "Find All Numbers Disappeared in an Array",
       link: "https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/",
       patterns: ["Arrays"],
+      topic: "Arrays",
       difficulty: "Easy"
     },
+    {
+      name: "Single Number",
+      link: "https://leetcode.com/problems/single-number/",
+      patterns: ["Arrays"],
+      topic: "Arrays",
+      difficulty: "Easy"
+    },
+    {
+      name: "Convert 1D Array Into 2D Array",
+      link: "https://leetcode.com/problems/convert-1d-array-into-2d-array/",
+      patterns: ["Arrays"],
+      topic: "Arrays",
+      difficulty: "Medium"
+    },
+    {
+      name: "Product of Array Except Self",
+      link: "https://leetcode.com/problems/product-of-array-except-self/",
+      patterns: ["Arrays"],
+      topic: "Arrays",
+      difficulty: "Medium"
+    },
+    {
+      name: "Find The Duplicate Number",
+      link: "https://leetcode.com/problems/find-the-duplicate-number/",
+      patterns: ["Two Pointers"],
+      topic: "Two Pointers",
+      difficulty: "Medium"
+    },
+    {
+      name: "Find All Duplicates in an Array",
+      link: "https://leetcode.com/problems/find-all-duplicates-in-an-array/",
+      patterns: ["Arrays"],
+      topic: "Arrays",
+      difficulty: "Medium"
+    },
+    {
+      name: "Set Matrix Zeroes",
+      link: "https://leetcode.com/problems/set-matrix-zeroes/",
+      patterns: ["Arrays"],
+      topic: "Arrays",
+      difficulty: "Medium"
+    },
+    {
+      name: "Spiral Matrix",
+      link: "https://leetcode.com/problems/spiral-matrix/",
+      patterns: ["Arrays"],
+      topic: "Arrays",
+      difficulty: "Medium"
+    },
+    {
+      name: "Rotate Image",
+      link: "https://leetcode.com/problems/rotate-image/",
+      patterns: ["Arrays"],
+      topic: "Arrays",
+      difficulty: "Medium"
+    },
+    {
+      name: "Longest Consecutive Sequence",
+      link: "https://leetcode.com/problems/longest-consecutive-sequence/",
+      patterns: ["Arrays"],
+      topic: "Arrays",
+      difficulty: "Medium"
+    },
+    {
+      name: "First Missing Positive",
+      link: "https://leetcode.com/problems/first-missing-positive/",
+      patterns: ["Arrays"],
+      topic: "Arrays",
+      difficulty: "Medium"
+    },
+    {
+      name: "",
+      link: "",
+      patterns: [],
+      topic: "",
+      difficulty: "Medium"
+    },
+    {
+      name: "Average of Levels in Binary Tree",
+      link: "",
+      patterns: ["Tree"],
+      topic: "Tree",
+      difficulty: "Medium"
+    },
   ];
+  
 /*
   {
     name: "",
@@ -28,3 +116,5 @@ const seanprashadList =
     difficulty: "Medium"
   },
   */
+
+  
