a4ce7486891b4a32f26148a6de37150d0965465d
Vasiliy Sushko <vasiliy.sushko@gmail.com>
2023-03-02 00:15:13 +0300

    added new problems

--- src/store/leetcode-data-structures-courses.js
+++ src/store/leetcode-data-structures-courses.js
@@ -126,7 +126,162 @@ export const LEETCODE_DATA_STRUCTURES_COURSES_LIST =
       patterns: [],
       difficulty: "Easy"
     },
+    {
+      name: "Binary Search",
+      link: "https://leetcode.com/problems/binary-search/",
+      topic: "Binary Search",
+      patterns: [],
+      difficulty: "Easy"
+    },
+    {
+      name: "Sqrt(x)",
+      link: "https://leetcode.com/problems/sqrtx/",
+      topic: "Binary Search",
+      patterns: [],
+      difficulty: "Easy"
+    },
+    {
+      name: "Guess Number Higher or Lower",
+      link: "https://leetcode.com/problems/guess-number-higher-or-lower/",
+      topic: "Binary Search",
+      patterns: [],
+      difficulty: "Easy"
+    },
+    {
+      name: "Search in Rotated Sorted Array",
+      link: "https://leetcode.com/problems/search-in-rotated-sorted-array/",
+      topic: "Binary Search",
+      patterns: [],
+      difficulty: "Easy"
+    },
+    {
+      name: "First Bad Version",
+      link: "https://leetcode.com/problems/first-bad-version/",
+      topic: "Binary Search",
+      patterns: [],
+      difficulty: "Easy"
+    },
+    {
+      name: "Find Peak Element",
+      link: "https://leetcode.com/problems/find-peak-element/",
+      topic: "Binary Search",
+      patterns: [],
+      difficulty: "Easy"
+    },
+    {
+      name: "Find Minimum in Rotated Sorted Array",
+      link: "https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/",
+      topic: "Binary Search",
+      patterns: [],
+      difficulty: "Easy"
+    },
+    {
+      name: "Find First and Last Position of Element in Sorted Array",
+      link: "https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/",
+      topic: "Binary Search",
+      patterns: [],
+      difficulty: "Easy"
+    },
+    {
+      name: "Find K Closest Elements",
+      link: "https://leetcode.com/problems/find-k-closest-elements/",
+      topic: "Binary Search",
+      patterns: [],
+      difficulty: "Easy"
+    },
+    {
+      name: "Closest Binary Search Tree Value",
+      link: "https://leetcode.com/problems/closest-binary-search-tree-value/",
+      topic: "Binary Search",
+      patterns: [],
+      difficulty: "Easy"
+    },
+    {
+      name: "Search in a Sorted Array of Unknown Size",
+      link: "https://leetcode.com/problems/search-in-a-sorted-array-of-unknown-size/",
+      topic: "Binary Search",
+      patterns: [],
+      difficulty: "Easy"
+    },
+    {
+      name: "Pow(x, n)",
+      link: "https://leetcode.com/problems/powx-n/",
+      topic: "Binary Search",
+      patterns: [],
+      difficulty: "Easy"
+    },
+    {
+      name: "Valid Perfect Square",
+      link: "https://leetcode.com/problems/valid-perfect-square/",
+      topic: "Binary Search",
+      patterns: [],
+      difficulty: "Easy"
+    },
+    {
+      name: "Find Smallest Letter Greater Than Target",
+      link: "https://leetcode.com/problems/find-smallest-letter-greater-than-target/",
+      topic: "Binary Search",
+      patterns: [],
+      difficulty: "Easy"
+    },
+    {
+      name: "Find Minimum in Rotated Sorted Array II",
+      link: "https://leetcode.com/problems/find-minimum-in-rotated-sorted-array-ii/",
+      topic: "Binary Search",
+      patterns: [],
+      difficulty: "Easy"
+    },
+    {
+      name: "Intersection of Two Arrays",
+      link: "https://leetcode.com/problems/intersection-of-two-arrays/",
+      topic: "Binary Search",
+      patterns: [],
+      difficulty: "Easy"
+    },
+    {
+      name: "Intersection of Two Arrays II",
+      link: "https://leetcode.com/problems/intersection-of-two-arrays-ii/",
+      topic: "Binary Search",
+      patterns: [],
+      difficulty: "Easy"
+    },
+    {
+      name: "Two Sum II - Input Array Is Sorted",
+      link: "https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/",
+      topic: "Binary Search",
+      patterns: [],
+      difficulty: "Easy"
+    },
+    {
+      name: "Find the Duplicate Number",
+      link: "https://leetcode.com/problems/find-the-duplicate-number/",
+      topic: "Binary Search",
+      patterns: [],
+      difficulty: "Easy"
+    },
+    {
+      name: "Median of Two Sorted Arrays",
+      link: "https://leetcode.com/problems/median-of-two-sorted-arrays/",
+      topic: "Binary Search",
+      patterns: [],
+      difficulty: "Easy"
+    },
+    {
+      name: "Find K-th Smallest Pair Distance",
+      link: "https://leetcode.com/problems/find-k-th-smallest-pair-distance/",
+      topic: "Binary Search",
+      patterns: [],
+      difficulty: "Easy"
+    },
+    {
+      name: "Split Array Largest Sum",
+      link: "https://leetcode.com/problems/split-array-largest-sum/",
+      topic: "Binary Search",
+      patterns: [],
+      difficulty: "Easy"
+    },
   ];
+  
 /*
     {
       name: "",
