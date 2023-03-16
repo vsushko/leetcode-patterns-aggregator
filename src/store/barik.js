12359900eef10c8d2342026e0c754abf33ca6e24
Vasiliy Sushko <vasiliy.sushko@gmail.com>
2023-03-17 02:40:29 +0300

    added new list

---
+++ src/store/barik.js
@@ -0,0 +1,10 @@
+export const  BARIK_LIST =
+  [
+    {
+      name: "Factorial Trailing Zeroes",
+      link: "https://leetcode.com/problems/factorial-trailing-zeroes/",
+      patterns: ["Integer"],
+      topic: "Math",
+      difficulty: "Medium"
+    },
+  ];
