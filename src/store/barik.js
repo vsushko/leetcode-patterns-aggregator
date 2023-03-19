9b95f69bbdd498731d56fa85efabab43c16e5531
Vasiliy Sushko <vasiliy.sushko@gmail.com>
2023-03-20 02:37:53 +0300

    added new probleem

--- src/store/barik.js
+++ src/store/barik.js
@@ -1,10 +1,17 @@
-export const  BARIK_LIST =
+export const BARIK_LIST =
   [
     {
       name: "Factorial Trailing Zeroes",
       link: "https://leetcode.com/problems/factorial-trailing-zeroes/",
       patterns: ["Integer"],
       topic: "Math",
+      difficulty: "Medium"
+    },
+    {
+      name: "Repeated DNA Sequences",
+      link: "https://leetcode.com/problems/repeated-dna-sequences/description/",
+      patterns: ["Arrays & Hashing"],
+      topic: "String",
       difficulty: "Medium"
     },
   ];
